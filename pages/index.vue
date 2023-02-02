4ef6a226dff3062ff85b98415971dbd7da458f25
sardor01 <sardor.astanov.01@mail.ru>
2023-02-02 20:47:15 +0500

    chore: pre-push hook added

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,8 @@
-<script setup lang="ts">
-const msg = ref('Nuxt3 Starter')
-msg.value = 1
-</script>
-
 <template>
   <div class="flex flex-col justify-center items-center text-center py-10">
     <img src="/nuxt.png" alt="Nuxt 3" class="w-18 h-18 inline-block">
     <h1 class="text-3xl font-semibold">
-      {{ msg }}
+      Nuxt3 Starter
     </h1>
   </div>
 </template>
