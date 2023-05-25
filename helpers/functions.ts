c6f8e1ca61900da43b0d136fb8477559d7f379cc
sardor01 <sardor.astanov.01@mail.ru>
2023-05-25 15:22:57 +0500

    feat: new helper functions added && full coverage

--- helpers/functions.ts
+++ helpers/functions.ts
@@ -1,13 +1,4 @@
-export const isObject = (value: unknown) => {
-  return value != null && typeof value === 'object' && Array.isArray(value) === false
-}
-
-export const stripTags = (input?: string) => {
-  if (!input) return ''
-  return input.replace(/<(?:.|\n)*?>/gm, '')
-}
-
-export const formatDate = (date?: string | number | Date, separator = '.') => {
+export const formatDate = (date?: string | number | Date, separator: '/' | '-' | '.' = '.') => {
   type numberOrString = number | string
 
   if (!date) date = new Date()
@@ -25,15 +16,58 @@ export const formatDate = (date?: string | number | Date, separator = '.') => {
   return `${dd}.${mm}.${yyyy}`
 }
 
-export const formatPhoneNumber = (phone: string) => {
-  return phone.replace(/[^0-9]/gi, '')
+export const formatPrice = (number: number, n = 2, x = 3, s = ',', c = '.') => {
+  const re = `\\d(?=(\\d{${x || 3}})+${n > 0 ? '\\D' : '$'})`
+  const num = number.toFixed(Math.max(0, ~~n))
+
+  return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), `$&${s || ','}`)
 }
 
-export const beautifyPhoneNumber = (phone: unknown) => {
-  if (!phone || typeof phone !== 'string') return ''
-  const cleaned = formatPhoneNumber(phone)
-  const match: any = cleaned.match(/^(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})$/)
-  return `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}-${match[5]}`
+export const getFileName = (path: string) => {
+  return path.replace(/^.*[\\\/]/, '')
+}
+
+export const getIdFromSlug = (slug: string) => {
+  return slug.split('-')[slug.split('-').length - 1]
+}
+
+export const getLocation = (href: string) => {
+  const match = href.match(
+    /^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)([\/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/,
+  )
+  return (
+    match && {
+      href,
+      protocol: match[1],
+      host: match[2],
+      hostname: match[3],
+      port: match[4],
+      pathname: match[5],
+      search: match[6],
+      hash: match[7],
+    }
+  )
+}
+
+export const getUid = (prefix = '') => {
+  const id = Date.now().toString(36) + Math.random().toString(36).substring(2)
+  return prefix ? `${prefix}-${id}` : id
+}
+
+export const getUrl = (arr: string[], index: number) => {
+  return arr.slice(0, index + 1).join('/') === '' ? '/' : `/${arr.slice(0, index + 1).join('/')}`
+}
+
+export const hyphenToCamelCase = (str: string) => {
+  return str.replace(/-./g, (m) => m[1].toUpperCase())
+}
+
+export const isObject = (value: unknown) => {
+  return value != null && typeof value === 'object' && Array.isArray(value) === false
+}
+
+export const isNullish = (val: any) => {
+  return [null, undefined, false].includes(val)
 }
 
 export const parseJSON = (value: string) => {
@@ -44,19 +78,25 @@ export const parseJSON = (value: string) => {
   }
 }
 
-export const getUid = (prefix = '') => {
-  const id = Date.now().toString(36) + Math.random().toString(36).substring(2)
-  return prefix ? `${prefix}-${id}` : id
+// #region phoneNumber
+export const formatPhoneNumber = (phone: string) => {
+  return phone.replace(/[^0-9]/gi, '')
 }
 
-export const getFileName = (path: string) => {
-  return path.replace(/^.*[\\\/]/, '')
+export const beautifyPhoneNumber = (phone: unknown) => {
+  if (!phone || typeof phone !== 'string') return ''
+  const cleaned = formatPhoneNumber(phone)
+  const match = cleaned.match(/^(\d{3})(\d{2})(\d{3})(\d{2})(\d{2})$/)
+  if (!match) return ''
+  return `+${match[1]} (${match[2]}) ${match[3]}-${match[4]}-${match[5]}`
+}
+// #endregion
+
+export const stripTags = (input?: string) => {
+  if (!input) return ''
+  return input.replace(/<(?:.|\n)*?>/gm, '')
 }
 
-export const getUrl = (arr: string[], index: number) => {
-  return arr.slice(0, index + 1).join('/') === '' ? '/' : arr.slice(0, index + 1).join('/')
-}
-
-export const hyphenToCamelCase = (str: string) => {
-  return str.replace(/-./g, (m) => m[1].toUpperCase())
+export const wait = <T>(time: number) => {
+  return new Promise<T>((resolve) => setTimeout(resolve, time))
 }
